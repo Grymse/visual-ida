497813f57305ab6a21970212d8f56d846b43d872
Nicolai Grymer <pronicoxd@gmail.com>
2025-06-10 19:24:19 +0200

    Rework MotionDetectionControl UI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
+	import { onMount } from 'svelte';
 	import '../app.css';
-
+	import { ModeWatcher, setMode } from 'mode-watcher';
+	onMount(() => {
+		setMode('dark');
+	});
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
 {@render children?.()}
